23e2b1380935726213bf157715665b474884fdd1
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-19 10:31:55 +0200

    Updated SearchSort component to include search functionality and sorting options based on user input.

--- src/components/Sort.jsx
+++ src/components/Sort.jsx
@@ -16,3 +16,19 @@ const SearchSort = ({ searchQuery, handleSearchChange, sortBy, handleSortChange 
     setSortState(criteria);
     handleSortChange(criteria);
   };
+  return (
+    <div className="search-sort-container">
+      <input type="text" placeholder=":mag:Search shows" value={searchQuery} onChange={handleSearchChange} />
+      <select id="sortDropdown" value={sortState} onChange={(e) => sortShows(e.target.value)}>
+        <option value="default" disabled>
+          Select an option
+        </option>
+        <option value="titleAZ">Title A-Z</option>
+        <option value="titleZA">Title Z-A</option>
+        <option value="dateUpdatedAscending">Date Updated (Ascending)</option>
+        <option value="dateUpdatedDescending">Date Updated (Descending)</option>
+      </select>
+    </div>
+  );
+};
+export default SearchSort;
