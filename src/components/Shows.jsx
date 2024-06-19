1b37c1b26442df15db9bf12cfb87697be2eddf53
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-19 09:22:01 +0200

    Created Shows component, added state for showData and seasonsData, and implement fetchData function in useEffect.

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -3,3 +3,13 @@ import React, { useState, useEffect } from "react";
 import Season from "../components/Season";
 import ShowBody from "../components/ShowsBody";
 
+export default function Shows() {
+    const [showData, setShowData] = useState(null);
+    const [seasonsData, setSeasonsData] = useState(null);
+
+    useEffect(() => {
+        fetchData();
+    
+    })
+
+    
