1034a4a07f3c1dd385606823294414984b90ae6d
Phillip Bogopane <pm.bogopane@gamil.com>
2024-06-19 07:34:12 +0200

    Added error handling for fetch request code.

--- src/components/Season.jsx
+++ src/components/Season.jsx
@@ -1,2 +1,35 @@
 import  {React, useState, useEffect } from "react";
 import SeasonBody from "../components/SeasonBody";
+export default function Shows(props) {
+
+    const [SeasonData, setSeasonData] = useState(null)
+    useEffect(() => {
+
+        // eslint-disable-next-line react/prop-types
+        if (props.seasonAPI) {
+
+            // eslint-disable-next-line react/prop-types
+            fetch(`https://podcast-api.netlify.app/id/${props.seasonAPI}`)
+                .then(response => response.json())
+                .then(data => {
+                    
+                    const getseasons = data.seasons
+                    const seasonsdata = getseasons.map((mapseasons) => {
+                        return (
+                            <SeasonBody
+                                key={mapseasons.seasons}
+                                {...mapseasons}
+                            />
+                        )
+                    })
+                    setSeasonData(seasonsdata)
+                })
+        }
+    // eslint-disable-next-line react/prop-types
+    }, [props.seasonAPI])
+    return (
+        <div>
+            {SeasonData}
+        </div>
+    )
+}
